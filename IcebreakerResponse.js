82535b599626c10b06d523b5a7815123b5ea57d9
Easwari Ramani <eshuprad@gmail.com>
2018-01-11 10:36:30 +0000

    automatically backed up by learn

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -1,4 +1,8 @@
 function IcebreakerResponse(email){
   this.email = email
 }
+IcebreakerResponse.BatchCreate=function(arrEmails){
+  for (cosnt email in arrEmails)
+  return new IcebreakerResponse(email);
+}
 
